9f208ddc3883f28b8b197bde05b495aa80b7a874
Manuel Osorio <manuelosorio@users.noreply.github.com>
2022-11-13 06:38:55 -0500

    Add node-fetch, enable esmodules, return exchange rates

--- netlify/functions/get-rate.js
+++ netlify/functions/get-rate.js
@@ -1,23 +1,42 @@
+import fetch from "node-fetch";
+import { HandlerEvent, HandlerContext, HandlerResponse } from "@netlify/functions"
+
 const API_KEY = process.env.API_KEY;
 
-exports.handler = async (event, _context) => {
-    const base = event.queryStringParameters.base;
-    const target = event.queryStringParameters.target;
+/**
+ *
+ * @param event {HandlerEvent}
+ * @param _context {HandlerContext}
+ * @returns {Promise<HandlerResponse>}
+ */
+export const handler = async (event, _context) => {
+    try {
+      let {base, target} = event.queryStringParameters;
+      base = !!base ? base : "USD";
+      return fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${API_KEY}&base=${base}&target=${target}`, {
+        method: 'GET'
+      }).catch((err) => {
+        console.error(err);
+        return {
+          statusCode: 400,
+          body: "Something went wrong"
+        };
+      }).then(async (res) => {
+        /**
+         *
+         * @type { CurrencyDataType }
+         */
+        const data = await res.json();
+        console.log(data);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data.exchange_rates[target])
+        };
+      });
+    } catch (e) {
+      console.error(e.message)
+    }
 
-  fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${API_KEY}&base=${base}&target=${target}`, {
-    method: 'GET'
-  }).catch((err) => {
-    console.error(err);
-    return {
-      statusCode: 400,
-      body: err.message
-    };
-  }).then(async (res) => {
-    const data = await res.json();
-    console.log(data);
-    return {
-      statusCode: 200,
-      body: "Working on it"
-    };
-  });
 }
+
+
---
+++ netlify/types/types.ts
@@ -0,0 +1,5 @@
+declare type CurrencyDataType = {
+  exchange_rates: {
+    [key: string]: number;
+  };
+}
