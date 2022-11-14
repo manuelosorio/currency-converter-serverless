df7b6ff53f8f6fa6a002ca0d20bb1fbddebb0e5d
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2022-11-14 05:14:30 -0500

    Disconnect from redis client after request is complete

--- netlify/functions/get-rate.js
+++ netlify/functions/get-rate.js
@@ -1,10 +1,8 @@
 const fetch = require('node-fetch');
 const redis = require('redis');
 
-
 const API_KEY = process.env.API_KEY;
-const CACHE_DURATION = process.env.CACHE_DURATION ?? 60 * 15;
-console.log(CACHE_DURATION)// in seconds
+const CACHE_DURATION = process.env.CACHE_DURATION ?? 60 * 15; // in seconds
 
 const redisConfig = {
   url: process.env.REDIS_URL,
@@ -13,41 +11,45 @@ const redisConfig = {
   username: process.env.REDIS_USERNAME
 }
 
-
 const client = redis.createClient(redisConfig);
-client.connect();
+client.on("connect", () => {
+  console.info("Client Connected");
+}).on("end", () => {
+  console.info("Client Disconnected");
+});
 
 /**
- *
+ * Serveless function handler for get-rate.
  * @param event {HandlerEvent}
  * @param _context {HandlerContext}
  * @returns {Promise<HandlerResponse>}
  */
 exports.handler = async (event, _context) => {
+    client.connect();
     try {
       let {base, target} = event.queryStringParameters;
       base = !!base ? base : "USD";
       const key = `${base}-${target}`;
       try {
         /**
-         *
+         * Attempts to create access the cache data if it exists
          * @type {RedisClientType<RedisDefaultModules & RedisModules, RedisFunctions, RedisScripts>}
          */
         const redisCache = await client.get(key);
         if (await redisCache) {
+          client.disconnect();
           return {
             statusCode: 200,
             body: redisCache
           }
         }
-      } catch (e) {
-        console.error(e);
+      } catch (err) {
+        console.error(err);
         return {
           statusCode: 400,
           body: JSON.stringify({errorMessage: "There was a Redis error"})
         };
       }
-
       return fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${API_KEY}&base=${base}&target=${target}`, {
         method: 'GET'
       }).catch((err) => {
@@ -67,25 +69,23 @@ exports.handler = async (event, _context) => {
          * @type {string}
          */
         const exchangeRateStr = JSON.stringify({rate: data.exchange_rates[target]});
-        client.set(key, exchangeRateStr, {
+        await client.set(key, exchangeRateStr, {
           EX: CACHE_DURATION,
           NX: true
-        })
+        }).finally(async () => {
+          await client.disconnect();
+        });
         return {
           statusCode: 200,
           body: exchangeRateStr
         };
       });
-    } catch (e) {
-      console.error(e.message)
+    } catch (err) {
       return {
         statusCode: 400,
         body: JSON.stringify({
-          errorMessage: e.message
+          errorMessage: err.message
         })
       }
     }
-
 }
-
-
